d898e5b8a31ba5112912f3b3a799d8b808fcce6e
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-01-02 22:23:32 +0200

    main function

--- main.c
+++ main.c
@@ -6,7 +6,24 @@
 /*   By: jony <jony@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/21 22:00:35 by jony              #+#    #+#             */
-/*   Updated: 2019/12/21 22:00:41 by jony             ###   ########.fr       */
+/*   Updated: 2020/01/02 22:21:13 by jony             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
+int		main(int argc, char **argv)
+{
+	char ***matrix;
+
+	if (argc != 2)
+		ft_putendl("usage: ./fillit source_file");
+	else
+	{
+		matrix = set_up(argv[1]);
+		if (!matrix)
+			return (1);
+		solve(matrix);
+	}
+	return (1);
+}
